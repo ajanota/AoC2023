344f5d6606fd4f71edbec32b0306e686ddafd357
Adam Janota <ajanota77@gmail.com>
2023-12-01 22:29:55 +0100

    Day 01 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,12 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        input.forEach{ line ->
+            sum += "${line.first { it.isDigit() }}${line.last { it.isDigit() }}".toInt()
+        }
+        return sum
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
 }
